6b978d37019aab23d4d853ff1afe4ed21c8567a3
islamzhumabaev <129617690+islamzhumabaev@users.noreply.github.com>
2024-09-08 18:12:15 +0500

    Add files via upload

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,22 @@
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Consumer {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, String> jsonToSend = new HashMap<>();
+        jsonToSend.put("name", "Islam");
+        jsonToSend.put("job", "Java-Backend");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonToSend);
+
+        String url="https://reqres.in/api/users/";
+        String response = restTemplate.postForObject(url,request, String.class);
+
+        System.out.println(response);
+    }
+}
